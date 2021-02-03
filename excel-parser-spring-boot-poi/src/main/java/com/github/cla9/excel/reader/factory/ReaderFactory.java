c1e62b04173a6c7ba27200271f6958ccafbc6e5d
iceflower <iceflower01@gmail.com>
2021-02-03 17:56:31 +0900

    ReaderFactory 클래스에, sheetName을 초기화 할 수 있는 로직을 추가하라

--- excel-parser-spring-boot-poi/src/main/java/com/github/cla9/excel/reader/factory/ReaderFactory.java
+++ excel-parser-spring-boot-poi/src/main/java/com/github/cla9/excel/reader/factory/ReaderFactory.java
@@ -10,42 +10,73 @@ import com.github.cla9.excel.reader.worker.WorkBookReader;
  * The type Reader factory.
  */
 public class ReaderFactory {
-    private final ExcelMetaModelMappingContext context;
 
-    /**
-     * Instantiates a new Reader factory.
-     *
-     * @param context the context
-     */
-    public ReaderFactory(ExcelMetaModelMappingContext context) {
-        this.context = context;
+  private final ExcelMetaModelMappingContext context;
+
+  /**
+   * Instantiates a new Reader factory.
+   *
+   * @param context the context
+   */
+  public ReaderFactory(ExcelMetaModelMappingContext context) {
+    this.context = context;
+  }
+
+  /**
+   * Create instance reader.
+   *
+   * @param <T>    the type parameter
+   * @param type   the type
+   * @param tClass the t class
+   * @return the reader
+   */
+  public <T> Reader<T> createInstance(ReaderType type, Class<T> tClass) {
+    final boolean isCached = context.hasMetaModel(tClass);
+    if (type == ReaderType.WORKBOOK) {
+      return isCached
+          ? new WorkBookReader<>(tClass, context.getMetaModel(tClass))
+          : new WorkBookReader<>(tClass);
     }
 
-    /**
-     * Create instance reader.
-     *
-     * @param <T>    the type parameter
-     * @param type   the type
-     * @param tClass the t class
-     * @return the reader
-     */
-    public <T> Reader<T> createInstance(ReaderType type, Class<T> tClass)  {
-        final boolean isCached = context.hasMetaModel(tClass);
-        if (type == ReaderType.WORKBOOK) {
-            return isCached ? new WorkBookReader<>(tClass, context.getMetaModel(tClass)) : new WorkBookReader<>(tClass);
-        }
-        return isCached ? new SAXReader<>(tClass, context.getMetaModel(tClass)) : new SAXReader<>(tClass);
+    return isCached
+        ? new SAXReader<>(tClass, context.getMetaModel(tClass))
+        : new SAXReader<>(tClass);
+  }
+
+  /**
+   * @param <T>       the type parameter
+   * @param type      the type
+   * @param sheetName target sheet name
+   * @param tClass    the t class
+   * @return the reader
+   */
+  public <T> Reader<T> createInstance(ReaderType type, String sheetName, Class<T> tClass) {
+    final boolean isCached = context.hasMetaModel(tClass);
+    if (type == ReaderType.WORKBOOK) {
+      return isCached
+          ? new WorkBookReader<>(tClass, sheetName, context.getMetaModel(tClass))
+          : new WorkBookReader<>(tClass, sheetName);
     }
 
-    /**
-     * Create instance reader.
-     *
-     * @param <T>    the type parameter
-     * @param tClass the t class
-     * @return the reader
-     */
-    public  <T> Reader<T> createInstance(Class<T> tClass){
-        final ExcelBody entity = tClass.getAnnotation(ExcelBody.class);
-        return createInstance(entity.type(), tClass);
+    return isCached
+        ? new SAXReader<>(tClass, sheetName, context.getMetaModel(tClass))
+        : new SAXReader<>(tClass, sheetName);
+  }
+
+  /**
+   * Create instance reader.
+   *
+   * @param <T>    the type parameter
+   * @param tClass the t class
+   * @return the reader
+   */
+  public <T> Reader<T> createInstance(Class<T> tClass) {
+    final ExcelBody entity = tClass.getAnnotation(ExcelBody.class);
+
+    if (entity.sheetName() != null) {
+      return createInstance(entity.type(), entity.sheetName(), tClass);
     }
+
+    return createInstance(entity.type(), tClass);
+  }
 }
